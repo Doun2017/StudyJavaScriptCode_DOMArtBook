d19d62e6cf35105434e499a754f05ad4c850cc90
DounWork <liushi2008wh@163.com>
2017-10-26 17:15:57 +0800

    下载压缩工具压缩了js代码，而且还有在线压缩的工具。

--- JavaScript DOM编程艺术(第2版)源码/chapter04/image_gallery/04practice/scripts/showPic.js
+++ JavaScript DOM编程艺术(第2版)源码/chapter04/image_gallery/04practice/scripts/showPic.js
@@ -1,20 +1,3 @@
-function showPic(whichpic) {
-  var source = whichpic.getAttribute("href");
-  var placeholder = document.getElementById("placeholder");
-  placeholder.setAttribute("src",source);
-  var text = whichpic.getAttribute("title");
-  var description = document.getElementById("description");
-  description.firstChild.nodeValue = text;
-}
 
-window.onload = function() {
-  if (!document.getElementById) return false;
-  if (!document.getElementsByTagName) return false;
-  var lnks = document.getElementsByTagName("a");
-  for (var i=0; i<lnks.length; i++) {
-	  lnks[i].onclick = function() {
-		showPic(this);
-		return false;
-	  }
-  }
-}
+function showPic(whichpic){var source=whichpic.getAttribute("href");var placeholder=document.getElementById("placeholder");placeholder.setAttribute("src",source);var text=whichpic.getAttribute("title");var description=document.getElementById("description");description.firstChild.nodeValue=text;}
+window.onload=function(){if(!document.getElementById)return false;if(!document.getElementsByTagName)return false;var lnks=document.getElementsByTagName("a");for(var i=0;i<lnks.length;i++){lnks[i].onclick=function(){showPic(this);return false;}}}
