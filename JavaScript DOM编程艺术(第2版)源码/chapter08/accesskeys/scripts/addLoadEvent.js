a3c63cb3aa4d92bb015abc97f714cf10e01a33ee
DounWork <liushi2008wh@163.com>
2017-10-31 17:30:15 +0800

    格式规整

--- JavaScript DOM编程艺术(第2版)源码/chapter08/accesskeys/scripts/addLoadEvent.js
+++ JavaScript DOM编程艺术(第2版)源码/chapter08/accesskeys/scripts/addLoadEvent.js
@@ -1,11 +1,11 @@
 function addLoadEvent(func) {
-  var oldonload = window.onload;
-  if (typeof window.onload != 'function') {
-    window.onload = func;
-  } else {
-    window.onload = function() {
-      oldonload();
-      func();
-    }
-  }
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function () {
+			oldonload();
+			func();
+		}
+	}
 }
